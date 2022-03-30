bd38df20ad7a504b24c0f57e87e73101af6730cd
Larissa Abboud <lariabboud@gmail.com>
2022-03-30 13:05:25 +0300

    created landing page

--- app/src/main/java/com/lausp/currency_converter_4/Page2activity.java
+++ app/src/main/java/com/lausp/currency_converter_4/Page2activity.java
@@ -2,7 +2,9 @@ package com.lausp.currency_converter_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Page2activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Page2activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2activity);
+    }
+    public void pageOne(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
