0f5fb5c6d4d9c6b31e9e82eda55178f20938c21a
Larissa Abboud <lariabboud@gmail.com>
2022-03-30 12:58:44 +0300

    added second activity

---
+++ app/src/main/java/com/lausp/currency_converter_4/Page2activity.java
@@ -0,0 +1,14 @@
+package com.lausp.currency_converter_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page2activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2activity);
+    }
+}
