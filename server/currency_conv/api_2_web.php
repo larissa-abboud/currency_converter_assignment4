40bed608b77b204a63b0b20ada03ca9858a765bf
Larissa Abboud <lariabboud@gmail.com>
2022-04-03 19:41:42 +0300

    Update api_2_web.php

--- server/currency_conv/api_2_web.php
+++ server/currency_conv/api_2_web.php
@@ -1,12 +1,12 @@
 <?php
-include("db_info.php")
+include("db_info.php");
 //include api web to obtain amount
 
 // insert in databse the rate and amount 
 // calculate the end result
 // return to front end as json object
-$query = $mysqli->prepare(SELECT* FROM rate);/*selct from table the rate and returns it as json object */
-$query->execute
+$query = $mysqli->prepare("SELECT* FROM rate;");/*selct from table the rate and returns it as json object */
+$query->execute();
 $array = $query->get_result(); // obtain the rate
 $response = [];//fetch
 while($rate = $array->fetch_assoc()){
