f1ceb81b8fbea82564ee82dab8ef52d0a47553b4
Larissa Abboud <lariabboud@gmail.com>
2022-04-03 21:17:40 +0300

    obtain rate from api_web

--- app/src/main/java/com/lausp/currency_converter_4/MainActivity.java
+++ app/src/main/java/com/lausp/currency_converter_4/MainActivity.java
@@ -2,7 +2,9 @@ package com.lausp.currency_converter_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,6 +14,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     EditText addAmount1;
     private TextView result;
@@ -20,6 +29,53 @@ public class MainActivity extends AppCompatActivity {
     EditText addAmount2;
     EditText rate;
     WebView view;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls){
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+            try{
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+            return result;
+        }
+
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try{
+                JSONObject json = new JSONObject(s);
+                String created_at = json.getString("created_at");
+                String joke = json.getString("value");
+                Log.i("Created At", created_at);
+                Log.i("Joke", joke);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 
 
 
@@ -45,7 +101,10 @@ public class MainActivity extends AppCompatActivity {
 
         String url = "http://localhost/currency_converter_assignment4/server/currency_conv/api_web.php";
         // php to AS
-        
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
+
+
 
 
     }
