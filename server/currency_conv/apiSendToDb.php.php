807f5509974045a30787452df50ca22f4108c05b
Larissa Abboud <lariabboud@gmail.com>
2022-04-03 20:51:37 +0300

    api to send data to db

---
+++ server/currency_conv/apiSendToDb.php.php
@@ -0,0 +1,19 @@
+<?php
+// send data from mobile to php
+include("db_info.php");
+
+
+$rate = $_GET["rate"] ??"" ;
+$amount_rec = $_GET["amount_rec"] ??"";
+
+$query = $mysqli->prepare("INSERT INTO rate (rate, amount_rec) VALUES (?, ?);");
+$query->bind_param("ii", $rate, $amount_rec);
+$query->execute();
+
+$response = [];
+$response["status"] = "obtained";
+echo $rate;
+$json_response = json_encode($response);
+echo $json_response;
+
+?>
